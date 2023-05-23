1ef50a279e34e6c1230eb5a3feb2d35a4bb30418
ElTiago <126792630+ElTiago@users.noreply.github.com>
2023-05-22 22:17:15 -0500

    Create Segundo_PuntoMain.java

---
+++ Actividad #3/Segundo_Punto/Segundo_PuntoMain.java
@@ -0,0 +1,11 @@
+package segundo_punto;
+
+public class Segundo_Punto {
+    
+    public static void main(String[] args) {
+        
+    VentanaPrincipal ventana = new VentanaPrincipal();
+    ventana.setVisible(true);
+    
+   }
+}
